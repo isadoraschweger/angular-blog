019b28980064ce0b6ddd78805ce6eb958130af14
isadoraschweger <isadoraschweger@hotmail.com>
2023-10-13 19:09:07 -0300

    content created

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,11 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
   styleUrls: ['./big-card.component.css']
 })
+
 export class BigCardComponent implements OnInit {
+
+@Input()
+  photoCover:string=""
+
+@Input()
+  cardTitle:string=""
+
+@Input()
+  Id:string="0"
+
+@Input()
+  cardDescription:string=""
+
 
   constructor() { }
 
---
+++ src/app/data/datafake.ts
@@ -0,0 +1,14 @@
+export const datafake = [
+  {
+    "id":"1",
+    "title": "Titulo",
+    "description": "textotextotexto",
+    "photoCover":"https://d2d7ho1ae66ldi.cloudfront.net/ArquivoNoticias/46fe32aa-7657-11ed-aa6e-9587410378a2/gil-galad-benjamin-walkers-galadriel-morfydd-clark-e-elrond-robert-aramayo-em-o-senhor-dos-aneis-os-aneis-de-poder-1661367549060_v2_1x1.jpg"
+  },
+  {
+    "id":"2",
+    "title": "NOVO HOMEM DE FERRO EM 3D",
+    "description": "marvel anuncia um novo filme do homem de ferro, confira",
+    "photoCover":""
+  }
+]
